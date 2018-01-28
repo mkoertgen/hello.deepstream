9b9943100e18335ad9d575a1cd6dace407b8aaac
Marcel Körtgen <marcel.koertgen@gmail.com>
2018-01-28 21:06:29 +0100

    (net) Add dotnetcore sample (wip)

---
+++ net/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using DeepStreamNet;
+
+namespace net
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new DeepStreamClient("localhost", 6020);
+
+            if (await client.LoginAsync())
+            {
+                Console.WriteLine("Logged in.");
+
+                var disp = await client.Events.SubscribeAsync("test", Console.WriteLine);
+                Console.WriteLine("Subscribed to 'test'.");
+
+                await Task.Delay(2000);
+
+                client.Events.Publish("test", "Hello World");
+                Console.WriteLine("Published 'test: Hello World'.");
+
+                await Task.Delay(3000);
+
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey();
+
+                await disp.DisposeAsync();
+            }
+
+            client.Dispose();
+
+            Console.WriteLine("Press any key to quit.");
+            Console.Read();
+        }
+    }
+}
