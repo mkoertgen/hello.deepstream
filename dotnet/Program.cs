57a60ca4aa57f2406e82411cb6e6efc7d1c73b96
Marcel Körtgen <marcel.koertgen@gmail.com>
2018-01-29 09:38:01 +0100

    (dotnet) Set file auth (not working yet)

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -4,44 +4,54 @@ using DeepStreamNet;
 
 namespace ConsoleSample
 {
-    class Program
+    internal class Program
     {
-        static async Task Main(string[] args)
+        public static async Task Main()
         {
             var url = GetEnv("DEEPSTREAM_URL", "localhost:6020").Split(':');
-            var dsHost = url[0];
-            var dsPort = int.Parse(url[1]);
+            var host = url[0];
+            var port = int.Parse(url[1]);
 
-            Console.WriteLine($"Connecting to DeepStream Server '{dsHost}:{dsPort}'...");
-            var client = new DeepStreamClient(dsHost, dsPort);
-
-            if (await client.LoginAsync())
+            Console.WriteLine($"Connecting to DeepStream Server '{host}:{port}'...");
+            using (var client = new DeepStreamClient(host, port))
             {
+                var user = GetEnv("DEEPSTREAM_USER", "userA");
+                var pwd = GetEnv("DEEPSTREAM_PWD", "rCOSZxJrgze2AZdVQh12c6ErDMOG0M+Rx5Yu7S5d91c=GS4SbTQYmoaGwjm2shEobg==");
+                var loggedIn = await client.LoginAsync(user, pwd);
+                if (!loggedIn) throw new InvalidOperationException($"Could not login with user '{user}'");
+
                 Console.WriteLine("Logged in.");
+                var eventName = GetEnv("DEEPSTREAM_EVENT", "test");
 
-                var disp = await client.Events.SubscribeAsync("test", Console.WriteLine);
-                Console.WriteLine("Subscribed to 'test'.");
+                // https://github.com/dotnet/roslyn/issues/114
+                //using (var disp = await client.Events.SubscribeAsync(eventName, Console.WriteLine))
+                var disp = await client.Events.SubscribeAsync(eventName, Console.WriteLine);
+                try
+                {
+                    Console.WriteLine($"Subscribed to '{eventName}'.");
 
-                await Task.Delay(2000);
+                    await Task.Delay(2000);
 
-                client.Events.Publish("test", "Hello World");
-                Console.WriteLine("Published 'test: Hello World'.");
+                    const string data = "Hello World";
+                    client.Events.Publish(eventName, data);
+                    Console.WriteLine($"Published '{eventName}: '{data}'.");
 
-                await Task.Delay(3000);
+                    await Task.Delay(3000);
 
-                Console.WriteLine("Press any key to continue.");
-                Console.ReadKey();
-
-                await disp.DisposeAsync();
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadKey();
+                }
+                finally
+                {
+                    await disp.DisposeAsync();
+                }
             }
-
-            client.Dispose();
 
             Console.WriteLine("Press any key to quit.");
             Console.Read();
         }
 
-        static string GetEnv(string name, string defaultValue)
+        private static string GetEnv(string name, string defaultValue)
         {
             var value = Environment.GetEnvironmentVariable(name);
             return value ?? defaultValue;
